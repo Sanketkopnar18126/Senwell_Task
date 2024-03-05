b06bee277e47fbad10a1870cf8709805d00e1523
sanket <sanketkopnar1299@gmail.com>
2024-03-05 12:54:20 +0530

    update and delete functionality dine

--- Backend/src/controllers/employ.controller.js
+++ Backend/src/controllers/employ.controller.js
@@ -39,20 +39,54 @@ const getEmploys = asynchHandler(async (req, res) => {
 });
 
 const updateEmploy = asynchHandler(async (req, res) => {
-   const { name } = req.body;
-   const employ = await Employ.findByIdAndUpdate(
-      req.params.id,
-      {
-         name,
-      },
-      {
-         new: true,
-      }
-   );
+   try {
+      const { name } = req.body;
 
-   return res
-      .status(200)
-      .json(new apiResponse(200, employ, "Successfully emply update"));
+      // Validate the request payload here if needed
+
+      const employ = await Employ.findByIdAndUpdate(
+         req.params.id,
+         { name },
+         { new: true }
+      );
+
+      if (!employ) {
+         return res
+            .status(404)
+            .json(new apiResponse(404, null, "Employee not found"));
+      }
+
+      return res
+         .status(200)
+         .json(new apiResponse(200, employ, "Successfully updated employee"));
+   } catch (error) {
+      console.error(error);
+      return res
+         .status(500)
+         .json(new apiResponse(500, null, "Internal Server Error"));
+   }
 });
-export { RegisterEmploy, getEmploys, updateEmploy };
+
+const deleteEmploy = asynchHandler(async (req, res) => {
+   try {
+      const employ = await Employ.findByIdAndDelete(req.params.id);
+      console.log("Employy delete", employ);
+      if (!employ) {
+         return res
+            .status(404)
+            .json(new apiResponse(404, null, "Employee not found"));
+      }
+
+      return res
+         .status(200)
+         .json(new apiResponse(200, employ, "Successfully deleted employee"));
+   } catch (error) {
+      console.error(error);
+      return res
+         .status(500)
+         .json(new apiResponse(500, null, "Internal Server Error"));
+   }
+});
+
+export { RegisterEmploy, getEmploys, updateEmploy, deleteEmploy };
 //  saurabhnayyar.senwell@gmail.com
--- Backend/src/routes/employ.routes.js
+++ Backend/src/routes/employ.routes.js
@@ -1,6 +1,7 @@
 import Router from "express";
 import {
    RegisterEmploy,
+   deleteEmploy,
    getEmploys,
    updateEmploy,
 } from "../controllers/employ.controller.js";
@@ -9,6 +10,7 @@ const router = Router();
 
 router.route("/register").post(RegisterEmploy);
 router.route("/get/:id").get(getEmploys);
-router.route('/update/"id').post(updateEmploy);
+router.route("/update/:id").post(updateEmploy);
+router.route("/delete/:id").post(deleteEmploy);
 
 export default router;
